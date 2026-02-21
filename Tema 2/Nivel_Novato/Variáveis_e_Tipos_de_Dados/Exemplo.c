a7a4d77015e14b4b301a39aeffe5d635e1196a51
Tainara Paula <147462577+Tainara-Paula@users.noreply.github.com>
2026-02-20 22:02:40 -0300

    Adicionar Exemplo.c
    Adicionando um arquivo com exemplos de Variáveis e Tipos de Dados. Nível Novato - Tema 2.

---
+++ Tema 2/Nivel_Novato/Variáveis_e_Tipos_de_Dados/Exemplo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main () {
+
+    /* Sintaxe da declaração de variáveis:
+    
+    Tipo_da_variável Nome_da_variável
+
+    */
+
+    /* Sintaxe da Inicialização de variáveis:
+    
+    Tipo_da_variável Nome_da_variável = Valor_da_variável
+
+    */
+
+    // Declaração / Inicialização:
+
+    int valor; // Declara uma variável inteira chamada "valor";
+    int idade = 25; // Declara uma variável inteira chamada "idade" e atribui o valor "25";
+    int quantidade = 1; // Declara uma variável inteira chamada "quantidade" e atribui o valor "1";
+    float altura = 1.75; // Declara uma variável de ponto flutuante chamada "altura" e atribui o valor "1.75";
+    double peso = 90.3; // Declara uma variável de ponto flutuante de dupla precisão chamada "peso" e atribui o valor "90.3";
+    char letra = 'A'; // Declara uma variável de caractere chamada "letra" e atribui o valor 'A';
+    char nome [20] = "Tainara"; // Declara uma variável de caractere string chamada "nome" e atribui o valor "Tainara";
+
+    valor = 25; // Anteriormente tinha-se declarado uma variável inteira chamada "valor" sem inicializa-lá, posteriormente atribui o valor "25" a ela;
+    quantidade = 10; // Anteriormente declarei e inicializei uma variável inteira chamada "quantidade" com o valor "1", posteriormente substitui o valor "1" pelo valor "10";
+  
+
+}
